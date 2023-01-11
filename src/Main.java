a66c17f247001ff277c59dee353c9524e8fcfd9b
Kostya Rakhimzyanov <kroshavr@mail.ru>
2023-01-11 21:54:49 +0500

    ДЗ сделано

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015,"Россия");
+        Car car1 = new Car("", "Granta", 1.7, "желтый", 2015,"Россия");
         Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
         Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
         Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
