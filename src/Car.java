aa9da5cf14a11a388b20a9f6cbe09d5e43c6b040
Kostya Rakhimzyanov <kroshavr@mail.ru>
2023-01-11 21:53:48 +0500

    ДЗ сделано

---
+++ src/Car.java
@@ -0,0 +1,84 @@
+public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        if (brand == "" || brand == "null") {
+            brand = "default";
+        }
+        return brand;
+    }
+
+    public void setBrand() {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        if (color == "" || color == "null") {
+            color = "белый";
+        }
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        if (year <= 0) {
+            year = 2000;
+        }
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return getBrand() + " " + getModel() + ", объем двигателя - " +
+                getEngineVolume() + "л, цвет кузова - " + getColor() +
+                ", год выпуска - " + getYear() + ", страна-производитель - " + getCountry();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import java.util.concurrent.Callable;
+
 public class Main {
     public static void main(String[] args) {
-    }
-}
+        Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015,"Россия");
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        Car car5 = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+        System.out.println(car3.toString());
+        System.out.println(car4.toString());
+        System.out.println(car5.toString());
+            }
+        }
