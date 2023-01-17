b87ed5196e36a40457e9ba0281fdefbcff84638b
Kostya Rakhimzyanov <kroshavr@mail.ru>
2023-01-17 20:18:44 +0500

    ДЗ

--- src/Car.java
+++ src/Car.java
@@ -1,43 +1,62 @@
 public class Car {
-    private String brand;
-    private String model;
+    final private String brand;
+    final private String model;
     private double engineVolume;
     private String color;
-    private int year;
-    private String country;
+    final private int year;
+    final private String country;
+    private String transmission;
+    final private String carBody;
+    private String numberOfTheCar;
+    final private int numberOfSeats;
+    private boolean  tires;
 
-    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
-    }
+    class Key {
+        final private boolean remoteEngineStart;
+        final private boolean keylessAccess;
 
-    public String getBrand() {
-        if (brand == "" || brand == "null") {
-            brand = "default";
+        public Key (boolean remoteEngineStart, boolean keylessAccess) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessAccess = keylessAccess;
         }
-        return brand;
+
+        @Override
+        public String toString() {
+            return "Удаленный запуск двигателя - " + remoteEngineStart +
+                    ", бесключевой доступ - " + keylessAccess;
+        }
     }
 
-    public void setBrand() {
-        this.brand = brand;
+    public Car(String brand, String model, double engineVolume, String color, int year, String country, String transmission,
+               String carBody, String numberOfTheCar,int numberOfSeats, boolean tires) {
+        this.brand = brand == null || brand.isBlank() ? "Default" : brand;
+        this.model = model;
+        this.engineVolume = engineVolume <= 0 ? 1.5 : engineVolume;
+        this.color = color == null || color.isBlank() ? "белый" : color;
+        this.year = year <= 0 ? 2000 : year;
+        this.country = country;
+        this.transmission = transmission == null || transmission.isBlank() ? "механический" : transmission;
+        this.carBody = carBody == null || carBody.isBlank() ? "седан" : carBody;
+        this.numberOfTheCar = numberOfTheCar == null || numberOfTheCar.isBlank() ? "Default" : numberOfTheCar;
+        this.numberOfSeats = numberOfSeats <= 0 ? 5 : numberOfSeats;
+        this.tires = tires;
+    }
+
+    public void choiceOfTires (int month) {
+        if (month >= 4 || month <= 10) {
+            setTires(false);
+        }
+        else setTires(true);
+    }
+    public String getBrand() {
+        return brand;
     }
 
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public double getEngineVolume() {
-        if (engineVolume <= 0) {
-            engineVolume = 1.5;
-        }
         return engineVolume;
     }
 
@@ -46,9 +65,6 @@ public class Car {
     }
 
     public String getColor() {
-        if (color == "" || color == "null") {
-            color = "белый";
-        }
         return color;
     }
 
@@ -57,28 +73,51 @@ public class Car {
     }
 
     public int getYear() {
-        if (year <= 0) {
-            year = 2000;
-        }
         return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
     }
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getCarBody() {
+        return carBody;
+    }
+
+    public String getNumberOfTheCar() {
+        return numberOfTheCar;
+    }
+
+    public void setNumberOfTheCar(String numberOfTheCar) {
+        this.numberOfTheCar = numberOfTheCar;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public boolean isTires() {
+        return tires;
+    }
+
+    public void setTires(boolean summer) {
+        this.tires = summer;
     }
 
     @Override
     public String toString() {
         return getBrand() + " " + getModel() + ", объем двигателя - " +
                 getEngineVolume() + "л., цвет кузова - " + getColor() +
-                ", год выпуска - " + getYear() + ", страна-производитель - " + getCountry();
+                ", год выпуска - " + getYear() + ", страна-производитель - " + getCountry() +
+                ", коробка передач - " + getTransmission() + ", тип кузова - " + getCarBody() +
+                ", номер мащины - " + getNumberOfTheCar() + ", количество мест - " + getNumberOfSeats() + ", резина -" + isTires();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
 import java.util.concurrent.Callable;
-
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("", "Granta", 1.7, "желтый", 2015,"Россия");
-        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
-        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
-        Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
-        Car car5 = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car car1 = new Car("", "Granta", 1.7, "желтый", 2015,"Россия", "Автомат", "седан", "ОИ458К", 5, true);
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия", "", "купе", "150", 3, true);
+        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия","автомат", "купе", "150", 3, true);
+        Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея", "","купе", "150", 3, true);
+        Car car5 = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея","механика", "купе", "150", 3, true);
+        Car.Key key1 = car1.new Key(false,false);
+        Car.Key key2 = car2.new Key(true,true);
+        Car.Key key3 = car3.new Key(true,true);
+        Car.Key key4 = car4.new Key(true,true);
+        Car.Key key5 = car5.new Key(true,true);
         System.out.println(car1.toString());
         System.out.println(car2.toString());
         System.out.println(car3.toString());
         System.out.println(car4.toString());
         System.out.println(car5.toString());
+        int month;
+        car1.choiceOfTires(6);
+        System.out.println(car1);
+        System.out.println(key1);
+
             }
         }
